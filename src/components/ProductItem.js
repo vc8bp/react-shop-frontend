233e14c37ee10526725acde1a479ad3dd6c34474
vc8bp <vc8bp3@gmail.com>
2023-01-23 19:06:08 +0530

    added ratings info of product in ProductItem component and handled err & res in getUserAdd comp

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -2,12 +2,12 @@ import { FavoriteBorderOutlined, SearchOutlined, ShoppingCartOutlined } from '@m
 import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom';
-import { mobile } from '../Responsive';
+import StarIcon from '@mui/icons-material/Star';
 
 
 const Wrapper = styled.div`
   display: flex;
-
+  position: relative; //becaus of retinsContainer
 `
 const Info = styled.div`
   padding: 10px; 
@@ -66,6 +66,28 @@ const Image = styled.img`
   object-fit: cover;
 `;
 
+//Rating
+const RatingsContainer = styled.div`
+  position: absolute;
+  bottom: 5px;
+  left: 10px;
+  background-color: rgba(235,240,245, 0.9);
+  box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);
+  display: flex;
+  align-items: center;
+  padding: 0px 5px;
+  border-radius: 5px;
+`;
+const Rating = styled.div`
+  padding: 5px;
+  display: flex;
+  align-items: center;
+  gap: 2px;
+`;
+const RatingCount = styled.div`
+  margin: 5px;
+`;
+
 const Container = styled.div`
   
   width: 210px;
@@ -79,7 +101,7 @@ const Container = styled.div`
   @media only screen and (min-width: 500px) {
     :hover{
       box-shadow: 0px 0px 15px #888888;
-      transform: scale(1.1);
+      transform: translateY(-5px);
     }
 
     :hover ${Title} {
@@ -108,15 +130,24 @@ const link = {
 
 function ProductItem(data) {
 
-  const { img, title, _id, desc, price} = data.data;
+  const { img, title, _id, desc, price, ratingsAverage, ratingsQuantity} = data.data;
 
   return (
     
       <Container>
         <Link style={link}  to={`/product/${_id}`}>
-        <Wrapper>
-          <Image src={img} /> 
-        </Wrapper>
+          <Wrapper>
+            <Image src={img} /> 
+            {ratingsQuantity ? 
+              (<RatingsContainer>
+                  <Rating>{ratingsAverage} <StarIcon style={{color: "teal", fontSize: "20px"}} /></Rating>
+                  <div style={{borderLeft: "1px solid green", height: "15px"}}></div>
+                  <RatingCount>
+                    {ratingsQuantity}
+                  </RatingCount>
+              </RatingsContainer>)
+            : null}
+          </Wrapper>
         </Link>
         <Info>
           <Title>{title}</Title>
